de95e3c71de86592ce9d0d2abab9d1eef4e7c4bc
James <james@Latitude-E5450-91a33ead.localdomain>
2022-06-14 18:50:33 +0200

    add index.js

--- index.js
+++ index.js
@@ -1,25 +1,22 @@
 const hamburger = document.querySelector('.icon');
-const navmenu =document.querySelector('.navbar-list');
-const about=document.querySelector('#nav-item');
-const portfolio=document.querySelector('#nav-item2');
-const contact =document.querySelector('#nav-item3')
+const navmenu = document.querySelector('.navbar-list');
+const about = document.querySelector('#nav-item');
+const portfolio = document.querySelector('#nav-item2');
+const contact = document.querySelector('#nav-item3');
 
-
-hamburger.addEventListener('click',show);
-about.addEventListener('click',close)
-portfolio.addEventListener('click',close)
-contact.addEventListener('click',close)
-
-
-function show()
-{
-   console.log("clicked me")
-   navmenu.style.top='0';
-   navmenu.style.left='10px';
-   navmenu.style.position='fixed'
-   navmenu.style.width='100%'
+function show() {
+  console.log('clicked me');
+  navmenu.style.top = '0';
+  navmenu.style.left = '10px';
+  navmenu.style.position = 'fixed';
+  navmenu.style.width = '100%';
 }
 
 function close() {
-    navmenu.style.top='-100%'
+  navmenu.style.top = '-100%';
 }
+
+hamburger.addEventListener('click', show);
+about.addEventListener('click', close);
+portfolio.addEventListener('click', close);
+contact.addEventListener('click', close);
