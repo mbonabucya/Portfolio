973d9e6e94451aa46a23f8c65f25ecba71f2ced1
James <james@Latitude-E5450-91a33ead.localdomain>
2022-06-15 04:59:50 +0200

    modify the index.js

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const navmenu = document.querySelector('.navbar-list');
 const about = document.querySelector('#nav-item');
 const portfolio = document.querySelector('#nav-item2');
 const contact = document.querySelector('#nav-item3');
-const closeMenu = document.querySelector('#closeMenu')
+const closeMenu = document.querySelector('#closeMenu');
 
 function show() {
   navmenu.style.top = '0';
@@ -20,4 +20,4 @@ hamburger.addEventListener('click', show);
 about.addEventListener('click', close);
 portfolio.addEventListener('click', close);
 contact.addEventListener('click', close);
-closeMenu.addEventListener('click', close)
+closeMenu.addEventListener('click', close);
