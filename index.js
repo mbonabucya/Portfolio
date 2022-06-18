ff60482d7d54a6d1e92710324df580961f4efae6
James <james@Latitude-E5450-91a33ead.localdomain>
2022-06-18 11:46:40 +0200

    remove form submit restriction

--- index.js
+++ index.js
@@ -206,14 +206,3 @@ function validateEmail() {
   newFlag = 1;
   return 1;
 }
-
-form.addEventListener('submit', (e) => {
-  e.preventDefault(); // prevent sumitting
-
-  validateEmail();
-  if (newFlag === 1) {
-    form.submit();
-  } else {
-    newFlag = 0;
-  }
-});
